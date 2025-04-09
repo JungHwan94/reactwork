d10e2dd71efd48eda6e7234f5ff2b8dffdf4216a
JungHwan94 <hjhwan94@naver.com>
2025-04-09 18:14:03 +0900

    2025 04 09 18:14

---
+++ 14.todolist/src/components/List.js
@@ -0,0 +1,34 @@
+import Todoitem from "./Todoitem";
+import { useState } from "react";
+
+const List = ({todos}) => {
+    const [search, setSearch] = useState('');
+
+    const getSearchData = () => {
+        if(search == "") {
+            return todos;
+        }
+        return todos.filter((todo) => 
+            todo.content.toLowerCase().includes(search.toLowerCase())
+        )
+    }
+
+    return (
+        <div className='List'>
+            <h4>Todo List😉</h4>
+            <input type="text" placeholder="검색어를 입력하세요" 
+                onChange={(e) => {
+                    setSearch(e.target.value);
+                }}/>
+            <div className='todos_wrapper'>
+                {
+                    getSearchData().map((todo) =>
+                        // <Todoitem todo = {todo} />
+                        <Todoitem {...todo} />
+                    )
+                }
+            </div>
+        </div>
+    )
+}
+export default List;
---
+++ 14.todolist/src/components/Todoitem.js
@@ -0,0 +1,24 @@
+/*
+const Todoitem = (props) => {
+    return (
+        <div className='Todoitem'>
+            <input type='checkbox' />
+            <span>{props.todo.content}</span> 
+            <span>{new Date(props.todo.date).toLocaleDateString()}</span>
+            <button>삭제</button>
+        </div>
+    )
+}
+*/
+
+const Todoitem = ({isDone, content, date}) => {
+  return (
+      <div className='Todoitem'>
+          <input type='checkbox' />
+          <span>{content}</span> 
+          <span>{new Date(date).toLocaleDateString()}</span>
+          <button>삭제💦</button>
+      </div>
+  )
+}
+export default Todoitem;
